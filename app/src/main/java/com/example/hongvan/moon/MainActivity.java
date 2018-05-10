61cc47044150994f1910537d94fa1b6a1ee16493
Hong Van Tan <sorawrsome@gmail.com>
2018-05-10 20:18:13 +0200

    Spinner im Startbildschirm

--- app/src/main/java/com/example/hongvan/moon/MainActivity.java
+++ app/src/main/java/com/example/hongvan/moon/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.hongvan.moon;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.hongvan.moon.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Button nxtbutton;
 
 
@@ -17,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Spinner spinner =findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Stadt, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
 
         nxtbutton = findViewById(R.id.button);
         nxtbutton.setOnClickListener(new View.OnClickListener() {
@@ -30,5 +44,16 @@ public class MainActivity extends AppCompatActivity {
     public void openAngebote(){
         Intent intent = new Intent(this, com.example.hongvan.moon.Angebote.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
