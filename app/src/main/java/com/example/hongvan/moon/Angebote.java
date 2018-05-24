aed91b24413efb28d38af7c8e9cd7a851c67e67f
TPN <thephongnguyen@hotmail.de>
2018-05-24 16:41:36 +0200

    RecyclerView

--- app/src/main/java/com/example/hongvan/moon/Angebote.java
+++ app/src/main/java/com/example/hongvan/moon/Angebote.java
@@ -3,8 +3,6 @@ package com.example.hongvan.moon;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,7 +20,7 @@ public class Angebote extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_angebote);
+        setContentView(R.layout.fragment_angebote);
         Log.d(TAG, "onCreate:started");
 
         prevButton = findViewById(R.id.prvbtn);
