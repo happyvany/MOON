6171034fccffd40eabf69138e654119ff7313644
TPN <thephongnguyen@hotmail.de>
2018-05-24 18:16:49 +0200

    RecyclerView

--- app/src/main/java/com/example/hongvan/moon/Angebote.java
+++ app/src/main/java/com/example/hongvan/moon/Angebote.java
@@ -1,26 +1,38 @@
 package com.example.hongvan.moon;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
-import java.util.ArrayList;
+import android.widget.FrameLayout;
 
 public class Angebote extends AppCompatActivity {
     private static final String TAG = "Angebote";
 
     //vars
-    private ArrayList<String> mNames = new ArrayList<>();
-    private ArrayList<String> mImageUrls = new ArrayList<>();
+
     private Button prevButton;
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private AngeboteFragment angeboteFragment;
+    private FavoritenFragment favoritenFragment;
+    private EinstellungenFragment einstellungenFragment;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_angebote);
+        setContentView(R.layout.kategorie);
         Log.d(TAG, "onCreate:started");
 
         prevButton = findViewById(R.id.prvbtn);
@@ -31,9 +43,42 @@ public class Angebote extends AppCompatActivity {
             }
         });
 
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.bottomn_navigation);
+
+        angeboteFragment = new AngeboteFragment();
+        favoritenFragment = new FavoritenFragment();
+        einstellungenFragment = new EinstellungenFragment();
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.nav_kat:
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(angeboteFragment);
+                        return true;
+                    case R.id.nav_fav:
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimaryDark);
+                        setFragment(favoritenFragment);
+                        return true;
+                    case R.id.nav_einst:
+                        mMainNav.setItemBackgroundResource(R.color.colorTheme);
+                        setFragment(einstellungenFragment);
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 
-
+    private void setFragment(Fragment fragment){
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+    }
 
 
 
